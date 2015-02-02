0ee923e6571290ca40b1ccc3b88447b7d37c7e14
Matthias Kestenholz <mk@spinlock.ch>
2015-02-02 21:45:48 +0100

    Add a base.html to showcase some bootstrap niceness

--- ftool/urls.py
+++ ftool/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views import generic
 
 
 urlpatterns = patterns(
@@ -7,6 +8,7 @@ urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'ftool.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', generic.TemplateView.as_view(template_name='base.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
