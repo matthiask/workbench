6f99550039c87f4fc6e290cfd7d08092b628d592
Matthias Kestenholz <mk@spinlock.ch>
2015-02-13 14:46:17 +0100

    Fix ProjectListView.get_queryset

--- projects/views.py
+++ projects/views.py
@@ -7,9 +7,6 @@ from tools.views import ListView
 class ProjectViewMixin(object):
     model = Project
 
-    def get_queryset(self):
-        return self.model.objects.all()
-
 
 class ProjectListView(ProjectViewMixin, ListView):
     model = Project
