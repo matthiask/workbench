c76912eb0b2ed268209797deb7cab44076d223c8
Matthias Kestenholz <mk@spinlock.ch>
2015-02-20 22:43:02 +0100

    Add a rendered services model

--- services/admin.py
+++ services/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from services.models import ServiceType
+from services.models import ServiceType, RenderedService
 
 
 admin.site.register(
     ServiceType,
     list_display=('title', 'billing_per_hour', 'position'),
     list_editable=('position',),
+)
+admin.site.register(
+    RenderedService,
+    list_display=(
+        'story', 'rendered_on', 'rendered_by', 'hours', 'description'),
+    raw_id_fields=('story', 'created_by', 'rendered_by'),
 )
--- services/models.py
+++ services/models.py
@@ -1,5 +1,10 @@
+from datetime import date
+
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
+from accounts.models import User
 
 
 class ServiceType(models.Model):
@@ -23,3 +28,43 @@ class ServiceType(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class RenderedService(models.Model):
+    story = models.ForeignKey(
+        'stories.Story',
+        verbose_name=_('story'),
+        related_name='renderedservices',
+    )
+    created_at = models.DateTimeField(
+        _('created at'),
+        default=timezone.now,
+    )
+    created_by = models.ForeignKey(
+        User,
+        verbose_name=_('created by'),
+        related_name='+',
+    )
+    rendered_on = models.DateField(
+        _('rendered on'),
+        default=date.today,
+    )
+    rendered_by = models.ForeignKey(
+        User,
+        verbose_name=_('rendered by'),
+        related_name='renderedservices',
+    )
+    hours = models.DecimalField(
+        _('hours'),
+        max_digits=5,
+        decimal_places=2,
+    )
+    description = models.TextField(_('description'))
+
+    class Meta:
+        ordering = ('-rendered_on', '-created_at')
+        verbose_name = _('rendered service')
+        verbose_name_plural = _('rendered services')
+
+    def __str__(self):
+        return self.description
