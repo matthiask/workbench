e09c03c43149acf444ffb6203be3ef58f73428d2
Matthias Kestenholz <mk@spinlock.ch>
2015-02-28 22:29:45 +0100

    Wire up the creation of rendered services

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
 from services.forms import RenderedServiceForm
 from stories.models import Story
 from tools.views import DetailView
@@ -10,7 +13,30 @@ class StoryMixin(object):
 class StoryDetailView(StoryMixin, DetailView):
     template_name_suffix = '_detail'
 
+    def get_form(self, data=None, files=None, **kwargs):
+        kwargs.setdefault('initial', {}).update({
+            'rendered_by': self.request.user.pk,
+        })
+        return RenderedServiceForm(data, files, **kwargs)
+
     def get_context_data(self, **kwargs):
-        return super().get_context_data(
-            form=RenderedServiceForm(),
-            **kwargs)
+        if 'form' not in kwargs:
+            kwargs['form'] = self.get_form()
+
+        return super().get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not self.allow_create():  # TODO RenderedService, not Story
+            return redirect('../')
+
+        form = self.get_form(request.POST, request.FILES)
+        if form.is_valid():
+            service = form.save(commit=False)
+            service.created_by = request.user
+            service.story = self.object
+            service.save()
+
+            return HttpResponse('Thanks', status=201)  # Created
+
+        return self.render_to_response(self.get_context_data(form=form))
