a9e3c3935f269bf5c0574a445f6e7864fbfa919e
Matthias Kestenholz <mk@spinlock.ch>
2015-03-04 09:08:37 +0100

    Include the initial version in the history

--- tools/history.py
+++ tools/history.py
@@ -5,12 +5,33 @@ from django.utils.translation import ugettext as _
 import reversion
 
 
-Change = namedtuple('Change', 'changes version')
+Change = namedtuple('Change', 'changes version number')
 
 
 def changes(instance, fields):
     versions = reversion.get_for_object(instance)[::-1]
     changes = []
+    version_changes = []
+    current_object = versions[0].object_version.object
+
+    for field in fields:
+        curr = getattr(current_object, field)
+        f = instance._meta.get_field(field)
+
+        version_changes.append(
+            _('Initial value of "%(field)s" was "%(current)s".')
+            % {
+                'field': f.verbose_name,
+                'current': curr,
+            }
+        )
+
+    changes.append(Change(
+        changes=version_changes,
+        version=versions[0],
+        number=1,
+    ))
+
     for previous, current in zip(versions, versions[1:]):
         version_changes = []
         previous_object = previous.object_version.object
@@ -43,5 +64,7 @@ def changes(instance, fields):
         changes.append(Change(
             changes=version_changes,
             version=current,
+            number=changes[-1].number + 1,
         ))
+
     return changes
