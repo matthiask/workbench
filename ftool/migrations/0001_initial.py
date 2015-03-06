92d78590c0f34d56277288fd3c5bc85345fde09c
Matthias Kestenholz <mk@spinlock.ch>
2015-03-06 11:08:37 +0100

    Add auditing trigger support

---
+++ ftool/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import io, os
+
+from django.conf import settings
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            io.open(
+                os.path.join(settings.BASE_DIR, 'tools', 'audit.sql')
+            ).read(),
+            None,
+        ),
+    ]
---
+++ ftool/models.py
@@ -0,0 +1,40 @@
+from django.contrib.postgres.fields import HStoreField
+from django.db import connections, models
+
+from psycopg2.extras import register_hstore
+
+
+class LoggedActionManager(models.Manager):
+    def register_hstore(self):
+        register_hstore(
+            connections['default'].cursor(),
+            globally=True)
+
+    def get_queryset(self, *args, **kwargs):
+        self.register_hstore()
+        return super().get_queryset(*args, **kwargs)
+
+
+class LoggedAction(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    schema_name = models.TextField()
+    table_name = models.TextField()
+    relid = models.IntegerField()
+    session_user_name = models.TextField(null=True)
+    action_tstamp_tx = models.DateTimeField()
+    action_tstamp_stm = models.DateTimeField()
+    transaction_id = models.IntegerField(null=True)
+    application_name = models.TextField(null=True)
+    client_addr = models.GenericIPAddressField(null=True)
+    client_port = models.IntegerField(null=True)
+    client_query = models.TextField(null=True)
+    action = models.TextField()
+    row_data = HStoreField(null=True)
+    changed_fields = HStoreField(null=True)
+    statement_only = models.BooleanField()
+
+    objects = LoggedActionManager()
+
+    class Meta:
+        managed = False
+        db_table = 'audit_logged_actions'
