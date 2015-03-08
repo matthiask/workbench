97a3d7c99cd81d43de29daebc1319d09facefd7e
Matthias Kestenholz <mk@spinlock.ch>
2015-03-08 15:58:22 +0100

    Add a detail modal for rendered services

--- ftool/templatetags/history.py
+++ ftool/templatetags/history.py
@@ -9,8 +9,13 @@ register = template.Library()
 
 
 @register.inclusion_tag('history.html')
-def history(instance, fields):
+def history(instance, fields=None):
+    if fields is None:
+        fields = [f.name for f in instance._meta.get_fields()]
+    else:
+        fields = re.split(r'[\s,]+', fields)
+
     return {
         'instance': instance,
-        'changes': changes(instance, re.split(r'[\s,]+', fields)),
+        'changes': changes(instance, fields),
     }
