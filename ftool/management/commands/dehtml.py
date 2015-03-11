bf533b89c319b384a365b2bd9e21753b5433267e
Matthias Kestenholz <mk@spinlock.ch>
2015-03-11 16:05:29 +0100

    Add a management command to remove HTML tags from description fields

---
+++ ftool/management/commands/dehtml.py
@@ -0,0 +1,62 @@
+from html.parser import HTMLParser
+import re
+
+from django.core.management.base import NoArgsCommand
+
+from offers.models import Offer
+from projects.models import Project
+
+
+class _DeHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.__text = []
+
+    def handle_data(self, data):
+        text = data.strip()
+        if len(text) > 0:
+            text = re.sub('[ \t\r\n]+', ' ', text)
+            self.__text.append(text + ' ')
+
+    def handle_entityref(self, name):
+        self.__text.append(self.unescape('&%s;' % name))
+
+    def handle_charref(self, name):
+        self.__text.append(self.unescape('&#%s;' % name))
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.__text.append('\n\n')
+        elif tag == 'br':
+            self.__text.append('\n')
+        elif tag == 'li':
+            self.__text.append('\n- ')
+
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'br':
+            self.__text.append('\n\n')
+
+    def text(self):
+        return ''.join(self.__text).strip()
+
+
+def dehtml(text):
+    try:
+        parser = _DeHTMLParser()
+        parser.feed(text)
+        parser.close()
+        return parser.text()
+    except:
+        return text
+
+
+class Command(NoArgsCommand):
+    help = 'De-htmlizes description fields'
+
+    def handle_noargs(self, **options):
+        for instance in Offer.objects.all():
+            instance.description = dehtml(instance.description)
+            instance.save(update_fields=('description',))
+        for instance in Project.objects.all():
+            instance.description = dehtml(instance.description)
+            instance.save(update_fields=('description',))
