373b138071782e92bbf9763b15a385fc7bcfe159
Matthias Kestenholz <mk@spinlock.ch>
2015-03-23 09:12:23 +0100

    Allow ./runtests.sh without an active virtualenv

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-coverage erase
-python -Wall venv/bin/coverage run --branch --include="*ftool*" --omit="*migrations*,*test_*,*venv*" ./manage.py test -v 2
-coverage html
+venv/bin/coverage erase
+venv/bin/python -Wall venv/bin/coverage run --branch --include="*ftool*" --omit="*migrations*,*test_*,*venv*" ./manage.py test -v 2
+venv/bin/coverage html
