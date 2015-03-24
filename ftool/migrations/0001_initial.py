523b89afc3171deb31de3decb57965bd72d7fd44
Matthias Kestenholz <mk@spinlock.ch>
2015-03-24 22:40:49 +0100

    Missing audit_audit_table calls

--- ftool/migrations/0001_initial.py
+++ ftool/migrations/0001_initial.py
@@ -19,4 +19,21 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(sql, None),
+        migrations.RunSQL(
+            "SELECT audit_audit_table('accounts_user');"
+            "SELECT audit_audit_table('activities_activity');"
+            "SELECT audit_audit_table('contacts_emailaddress');"
+            "SELECT audit_audit_table('contacts_organization');"
+            "SELECT audit_audit_table('contacts_person');"
+            "SELECT audit_audit_table('contacts_phonenumber');"
+            "SELECT audit_audit_table('contacts_postaladdress');"
+            "SELECT audit_audit_table('deals_deal');"
+            "SELECT audit_audit_table('invoices_invoice');"
+            "SELECT audit_audit_table('offers_offer');"
+            "SELECT audit_audit_table('projects_project');"
+            "SELECT audit_audit_table('projects_release');"
+            "SELECT audit_audit_table('stories_renderedservice');"
+            "SELECT audit_audit_table('stories_requiredservice');"
+            "SELECT audit_audit_table('stories_story');",
+            ""),
     ]
