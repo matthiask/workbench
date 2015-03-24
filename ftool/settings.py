e60ba83a65a93fc1732ce6489eccc14afe50e720
Matthias Kestenholz <mk@spinlock.ch>
2015-03-24 18:02:43 +0100

    Activity detail template

--- ftool/settings.py
+++ ftool/settings.py
@@ -70,7 +70,9 @@ WSGI_APPLICATION = 'wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'ftool', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
