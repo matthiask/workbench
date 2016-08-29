08e1fce066f85b6b3e69c9c38da8ed4d7b234e41
Matthias Kestenholz <mk@feinheit.ch>
2016-08-29 17:06:02 +0200

    Add language

--- workbench/settings.py
+++ workbench/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [{
     'OPTIONS': {
         'context_processors': [
             'django.template.context_processors.debug',
+            'django.template.context_processors.i18n',
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
