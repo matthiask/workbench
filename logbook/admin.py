9c823f3cc6e71f57cc73b8f7324c414fcf58a4ee
Matthias Kestenholz <mk@feinheit.ch>
2016-09-23 22:40:54 +0200

    Add logged costs

--- logbook/admin.py
+++ logbook/admin.py
@@ -5,10 +5,19 @@ from logbook import models
 
 class LoggedHoursAdmin(admin.ModelAdmin):
     list_display = (
-        'task', 'created_at', 'created_by', 'rendered_by', 'hours',
-        'description',
+        'task', 'created_at', 'created_by', 'rendered_on', 'rendered_by',
+        'hours', 'description',
     )
     raw_id_fields = ('task', 'invoice')
 
 
+class LoggedCostAdmin(admin.ModelAdmin):
+    list_display = (
+        'project', 'service', 'created_by', 'rendered_on',
+        'cost', 'description',
+    )
+    raw_id_fields = ('project', 'service', 'invoice')
+
+
 admin.site.register(models.LoggedHours, LoggedHoursAdmin)
+admin.site.register(models.LoggedCost, LoggedCostAdmin)
--- logbook/models.py
+++ logbook/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 from accounts.models import User
-from projects.models import Task
+from offers.models import Service
+from projects.models import Project, Task
 from tools.models import SearchManager, Model
 from tools.urls import model_urls
 
@@ -67,6 +68,70 @@ class LoggedHours(Model):
         ordering = ('-rendered_on', '-created_at')
         verbose_name = _('logged hours')
         verbose_name_plural = _('logged hours')
+
+    def __str__(self):
+        return self.description
+
+
+@model_urls()
+class LoggedCost(Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.PROTECT,
+        related_name='loggedcosts',
+        verbose_name=_('project'),
+    )
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.PROTECT,
+        related_name='loggedcosts',
+        verbose_name=_('service'),
+    )
+
+    created_at = models.DateTimeField(
+        _('created at'),
+        default=timezone.now,
+    )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='+',
+        verbose_name=_('created by'),
+    )
+    rendered_on = models.DateField(
+        _('rendered on'),
+        default=date.today,
+    )
+    cost = models.DecimalField(
+        _('cost'),
+        max_digits=10,
+        decimal_places=2,
+        help_text=_('The amount we did have to pay.'),
+    )
+    description = models.TextField(
+        _('description'),
+    )
+
+    invoice = models.ForeignKey(
+        'invoices.Invoice',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        verbose_name=_('invoice'),
+        related_name='+',
+    )
+    archived_at = models.DateTimeField(
+        _('archived at'),
+        blank=True,
+        null=True,
+    )
+
+    objects = SearchManager()
+
+    class Meta:
+        ordering = ('-rendered_on', '-created_at')
+        verbose_name = _('logged cost')
+        verbose_name_plural = _('logged cost')
 
     def __str__(self):
         return self.description
