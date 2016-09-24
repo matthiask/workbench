f6f1c377aa385c2a01ae1a7b084deca4a78e96cd
Matthias Kestenholz <mk@feinheit.ch>
2016-09-24 22:08:43 +0200

    Show type

--- invoices/admin.py
+++ invoices/admin.py
@@ -6,9 +6,9 @@ from invoices import models
 class InvoiceAdmin(admin.ModelAdmin):
     date_hierarchy = 'invoiced_on'
     list_display = (
-        'title', 'customer', 'project', 'invoiced_on', 'owned_by', 'status',
-        'total')
-    list_filter = ('status',)
+        'title', 'customer', 'project', 'invoiced_on', 'owned_by', 'type',
+        'status', 'total')
+    list_filter = ('type', 'status',)
     raw_id_fields = (
         'customer', 'contact', 'project', 'down_payment_applied_to')
 
