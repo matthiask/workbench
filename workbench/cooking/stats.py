641074ecbdafcbc5cb7949854d902dbc81a063ee
Matthias Kestenholz <mk@feinheit.ch>
2018-11-20 22:45:26 +0100

    Better stats which only looks at people having presence

--- workbench/cooking/stats.py
+++ workbench/cooking/stats.py
@@ -1,4 +1,4 @@
-from collections import Counter, defaultdict
+from collections import Counter
 
 from workbench.accounts.models import User
 
@@ -6,26 +6,39 @@ from .models import Day, Presence
 
 
 def run(year=2019):
-    users = User.objects.all()
-
     counts = Counter(
         Day.objects.filter(day__year=year).values_list("handled_by", flat=True)
     )
-    counts_sum = sum(counts.values())
-    presences = defaultdict(
-        lambda: 100,
-        Presence.objects.filter(year=year).values_list("user", "percentage"),
+    presences = dict(
+        Presence.objects.filter(year=year).values_list("user", "percentage")
     )
-    presences_sum = sum((presences[user.id] for user in users), 0)
+
+    users = User.objects.filter(id__in=set(counts.keys()) | set(presences))
+    presences_sum = sum(presences.values())
+    counts_sum = sum(
+        (
+            count
+            for user_id, count in counts.items()
+            if user_id in presences or user_id is None
+        ),
+        0,
+    )
+
+    print(
+        "{} Tage gesamt, davon {} zu verteilen auf {}% Präsenz".format(
+            sum(counts.values()), counts_sum, presences_sum
+        )
+    )
 
     for user in users:
-        reached = presences[user.id] / presences_sum * counts_sum
+        presence = presences.get(user.id, 0)
+        reached = presence / presences_sum * counts_sum
         print(
             "{:>20.20}: {:>3}%, Soll {:>3}, Ist {:>3}, Erreichung {}%".format(
                 user.get_full_name(),
-                presences[user.id],
+                presence,
                 round(reached),
                 counts[user.id],
-                round(100 * counts[user.id] / reached),
+                round(100 * counts[user.id] / reached) if reached else "-",
             )
         )
