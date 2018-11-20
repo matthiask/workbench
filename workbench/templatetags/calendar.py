8e37b74b40cd7ff550a48b1c294bab3359f3cc63
Matthias Kestenholz <mk@feinheit.ch>
2018-11-20 21:50:02 +0100

    Layout

--- workbench/templatetags/calendar.py
+++ workbench/templatetags/calendar.py
@@ -9,14 +9,14 @@ def calendar(days):
     if not days:
         return
 
-    week = [None] * 7
+    week = [None] * 5
     key = days[0].day.isocalendar()[:2]
     previous = days[0].day
     for day in days:
         if day.day.isocalendar()[:2] != key:
             key = day.day.isocalendar()[:2]
             yield previous, week[:]
-            week[:] = [None] * 7
+            week[:] = [None] * 5
         week[day.day.weekday()] = day
         previous = day.day
     yield previous, week[:]
