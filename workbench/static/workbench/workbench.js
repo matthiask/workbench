66d17623d9ee1e34377dca9a0755e96025812767
Matthias Kestenholz <mk@feinheit.ch>
2019-02-27 23:21:29 +0100

    Much much simpler date picker

--- workbench/static/workbench/workbench.js
+++ workbench/static/workbench/workbench.js
@@ -123,23 +123,11 @@ $(function() {
 });
 
 function initWidgets() {
-  $('.datepicker:not(.has-datepicker)').each(function() {
-    var el = this;
-    el.setAttribute("data-toggle", "datetimepicker");
-    el.setAttribute("data-target", "#" + el.getAttribute("id"));
-    el.classList.add("has-datepicker")
-    $(el).datetimepicker({
-      locale: 'de',
-      format: 'DD.MM.YYYY',
-      useCurrent: false,
-      keepOpen: false,
-      buttons: {
-        showToday: true,
-      },
-      icons: {
-        today: 'glyphicon glyphicon-calendar set-to-today'
-      }
-    });
+  $('.datepicker:not(.has-datepicker)').addClass("has-datepicker").datepicker({
+    language: "de-DE",
+    format: "dd.mm.yyyy",
+    autoHide: true,
+    autoPick: true,
   });
 
   var invoicedOn = $('#id_invoiced_on');
