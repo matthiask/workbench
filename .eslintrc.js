ad0012000fcd2b5f8d436e1dd9283ae0c2fc7717
Matthias Kestenholz <mk@feinheit.ch>
2019-03-01 09:24:28 +0100

    Prettier, ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: ["prettier"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_|event" }],
+    "prettier/prettier": "error",
+    quotes: 0
+  }
+};
--- workbench/static/workbench/workbench.js
+++ workbench/static/workbench/workbench.js
@@ -1,10 +1,11 @@
 $(function() {
-
   // AJAX modals
   var dismissModals = function() {
     // LOL, dismiss.
-    $('.modal, .modal-backdrop').remove();
-    $(document.body).removeClass('modal-open').removeAttr('style');
+    $(".modal, .modal-backdrop").remove();
+    $(document.body)
+      .removeClass("modal-open")
+      .removeAttr("style");
   };
 
   var initModal = function(data) {
@@ -13,19 +14,22 @@ $(function() {
     $(data).modal();
 
     setTimeout(function() {
-      var fields = $('.modal').find('input, select');
+      var fields = $(".modal").find("input, select");
       if (fields.filter("[autofocus]").length) {
         fields.filter("[autofocus]").focus();
       } else {
-        fields.filter(':visible').first().focus();
+        fields
+          .filter(":visible")
+          .first()
+          .focus();
       }
     }, 500);
 
     initWidgets();
   };
 
-  $(document.body).on('click', '[data-toggle]', function(event) {
-    if (this.dataset.toggle == 'ajaxmodal') {
+  $(document.body).on("click", "[data-toggle]", function(event) {
+    if (this.dataset.toggle == "ajaxmodal") {
       event.preventDefault();
       $.get(this.href, function(data) {
         initModal(data);
@@ -33,30 +37,34 @@ $(function() {
     }
 
     var el = $(this.dataset.toggle);
-    if (el.length) el.toggleClass('toggled');
+    if (el.length) el.toggleClass("toggled");
   });
 
   // Include the name and value of the submit button
-  $(document.body).on('click', ':submit', function(event) {
+  $(document.body).on("click", ":submit", function(event) {
     var el = $(this),
-      form = el.parents('form');
-    form.find('.hidden-submit-value').remove();
-    if (!form.length || !el.attr('name')) {
+      form = el.parents("form");
+    form.find(".hidden-submit-value").remove();
+    if (!form.length || !el.attr("name")) {
       return;
     }
     form.append(
       $('<input type="hidden" class="hidden-submit-value">').attr({
-        name: el.attr('name'),
-        value: el.attr('value')
+        name: el.attr("name"),
+        value: el.attr("value")
       })
     );
   });
 
-  $(document.body).on('submit', '.modal-dialog form', function(event) {
-    if (this.method.toLowerCase() == 'post') {
+  $(document.body).on("submit", ".modal-dialog form", function(event) {
+    if (this.method.toLowerCase() == "post") {
       $.post(this.action, $(this).serialize(), function(data, status, jqXHR) {
         // 201 CREATED, 202 ACCEPTED or 204 NO CONTENT
-        if (jqXHR.status === 201 || jqXHR.status === 202 || jqXHR.status === 204) {
+        if (
+          jqXHR.status === 201 ||
+          jqXHR.status === 202 ||
+          jqXHR.status === 204
+        ) {
           dismissModals();
           window.location.reload();
         } else {
@@ -64,52 +72,70 @@ $(function() {
         }
       });
     } else {
-      $.get(this.action + '?' + $(this).serialize(), function(data, status, jqXHR) {
+      $.get(this.action + "?" + $(this).serialize(), function(
+        data /*,
+        status,
+        jqXHR
+        */
+      ) {
         initModal(data);
       });
     }
     return false;
   });
 
-  $(document.body).on('click', '[data-toggle=picker]', function() {
+  $(document.body).on("click", "[data-toggle=picker]", function() {
     var el = $(this),
-      id = el.data('id'),
-      key = el.data('key'),
-      pretty = el.data('pretty');
+      id = el.data("id"),
+      key = el.data("key"),
+      pretty = el.data("pretty");
 
-    $('#' + id + '_pretty').val(pretty);
-    $('#' + id).val(key).trigger('change');
+    $("#" + id + "_pretty").val(pretty);
+    $("#" + id)
+      .val(key)
+      .trigger("change");
     dismissModals();
   });
 
   // Search forms
-  $('.form-search').on('change', 'select, input', function() {
-    $(this).closest('form').submit();
+  $(".form-search").on("change", "select, input", function() {
+    $(this)
+      .closest("form")
+      .submit();
   });
 
-
   // Hotkeys
-  $(document.body).on('keydown', function(e) {
+  $(document.body).on("keydown", function(e) {
     if (/Mac/.test(navigator.platform) ? !e.ctrlKey : !e.altKey) {
       return;
     }
 
-    if (e.keyCode === 70) {  // f
-      $('.navbar-form input[name=q]').focus().select();
-    } else if (e.keyCode === 72) {  // h
-      window.location.href = '/';
-    } else if (e.keyCode === 67 && !e.shiftKey) {  // c
-      window.location.href = '/contacts/people/';
-    } else if (e.keyCode === 67 && e.shiftKey) {  // C
-      window.location.href = '/contacts/organizations/';
-    } else if (e.keyCode === 80) {  // p
-      window.location.href = '/projects/';
-    } else if (e.keyCode === 79) {  // o
-        window.location.href = '/offers/';
-    } else if (e.keyCode === 82) {  // r
-      window.location.href = '/invoices/';
-    } else if (e.keyCode === 65) {  // a
-      window.location.href = '/activities/';
+    if (e.keyCode === 70) {
+      // f
+      $(".navbar-form input[name=q]")
+        .focus()
+        .select();
+    } else if (e.keyCode === 72) {
+      // h
+      window.location.href = "/";
+    } else if (e.keyCode === 67 && !e.shiftKey) {
+      // c
+      window.location.href = "/contacts/people/";
+    } else if (e.keyCode === 67 && e.shiftKey) {
+      // C
+      window.location.href = "/contacts/organizations/";
+    } else if (e.keyCode === 80) {
+      // p
+      window.location.href = "/projects/";
+    } else if (e.keyCode === 79) {
+      // o
+      window.location.href = "/offers/";
+    } else if (e.keyCode === 82) {
+      // r
+      window.location.href = "/invoices/";
+    } else if (e.keyCode === 65) {
+      // a
+      window.location.href = "/activities/";
     } else if (e.keyCode === 76) {
       var el = document.querySelector("[data-createhours]");
       if (el) {
@@ -120,9 +146,11 @@ $(function() {
         alert("Bitte zuerst Projekt auswählen");
       }
     } else if (e.keyCode === 13) {
-      $(e.target).parents('form').submit();
+      $(e.target)
+        .parents("form")
+        .submit();
     } else {
-      console.log(e, e.keyCode);
+      window.console && window.console.log(e, e.keyCode);
       return;
     }
 
@@ -131,49 +159,54 @@ $(function() {
 
   // Widgets
   initWidgets();
-
 });
 
 function initWidgets() {
-  $('.datepicker:not(.has-datepicker)').addClass("has-datepicker").datepicker({
-    language: "de-DE",
-    autoHide: true,
-    zIndex: 1500,
-  });
+  $(".datepicker:not(.has-datepicker)")
+    .addClass("has-datepicker")
+    .datepicker({
+      language: "de-DE",
+      autoHide: true,
+      zIndex: 1500
+    });
 
   function addZero(num) {
     return num < 10 ? "0" + num : "" + num;
   }
 
-  var invoicedOn = $('#id_invoiced_on');
-  var dueOn = $('#id_due_on');
+  var invoicedOn = $("#id_invoiced_on");
+  var dueOn = $("#id_due_on");
   if (invoicedOn.length && dueOn.length) {
-    invoicedOn.on('change', function(e) {
-      var due = new Date(invoicedOn.datepicker("getDate").getTime() + 14 * 86400 * 1000);
+    invoicedOn.on("change", function(event) {
+      var due = new Date(
+        invoicedOn.datepicker("getDate").getTime() + 14 * 86400 * 1000
+      );
       dueOn.val(
-        addZero(due.getDate()) + "." +
-        addZero(1 + due.getMonth()) + "." +
-        addZero(due.getFullYear())
+        addZero(due.getDate()) +
+          "." +
+          addZero(1 + due.getMonth()) +
+          "." +
+          addZero(due.getFullYear())
       );
     });
   }
 }
 
-function addInlineForm(slug, onComplete) {
-  var totalForms = $('#id_' + slug + '-TOTAL_FORMS'),
+window.addInlineForm = function addInlineForm(slug, onComplete) {
+  var totalForms = $("#id_" + slug + "-TOTAL_FORMS"),
     newId = parseInt(totalForms.val());
 
   totalForms.val(newId + 1);
-  var empty = $('#' + slug + '-empty'),
-    attributes = ['id', 'name', 'for'],
+  var empty = $("#" + slug + "-empty"),
+    attributes = ["id", "name", "for"],
     form = $(empty.html());
 
-  form.removeClass('empty').attr('id', slug + '-' + newId);
+  form.removeClass("empty").attr("id", slug + "-" + newId);
 
-  for (var i=0; i<attributes.length; ++i) {
+  for (var i = 0; i < attributes.length; ++i) {
     var attr = attributes[i];
 
-    form.find('*[' + attr + '*=__prefix__]').each(function() {
+    form.find("*[" + attr + "*=__prefix__]").each(function() {
       var el = $(this);
       el.attr(attr, el.attr(attr).replace(/__prefix__/, newId));
     });
@@ -182,12 +215,12 @@ function addInlineForm(slug, onComplete) {
   // insert the form after the last sibling with the same tagName
   // cannot use siblings() here, because the empty element may be the
   // only one (if no objects exist until now)
-  form.insertAfter(
-    empty.parent().children('[id|=' + slug + ']:last')
-    ).hide().fadeIn();
+  form
+    .insertAfter(empty.parent().children("[id|=" + slug + "]:last"))
+    .hide()
+    .fadeIn();
 
-  if (onComplete)
-    onComplete(form);
+  if (onComplete) onComplete(form);
 
   return false;
 };
