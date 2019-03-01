08b25987cfdef01e6b141fd29abb8b963803779f
Matthias Kestenholz <mk@feinheit.ch>
2019-03-01 09:06:19 +0100

    Add a base class for adding XLSX reporting helpers

---
+++ workbench/tools/xlsx.py
@@ -0,0 +1,18 @@
+from xlsxdocument import XLSXDocument
+
+
+class WorkbenchXLSXDocument(XLSXDocument):
+    def logged_hours(self, queryset):
+        self.table_from_queryset(
+            queryset.select_related(
+                "rendered_by", "service__project__owned_by", "invoice"
+            ),
+            additional=[
+                ("service", lambda hours: hours.service),
+                ("project", lambda hours: hours.service.project),
+                ("invoice", lambda hours: hours.invoice),
+            ],
+        )
+
+    def invoices(self, queryset):
+        self.table_from_queryset(queryset)
