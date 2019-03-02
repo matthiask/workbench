bcae4e7428bb1cc1b22d76ddc305fc301ff3cc88
Matthias Kestenholz <mk@feinheit.ch>
2019-03-02 09:12:18 +0100

    Spice up the main nav

---
+++ workbench/logbook/__init__.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+default_app_config = "workbench.logbook.Config"
+
+
+class Config(AppConfig):
+    name = "workbench.logbook"
+    verbose_name = _("logbook")
