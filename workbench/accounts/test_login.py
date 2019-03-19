9586b278e7148f72ef706daf47e23d0819bdd8c5
Matthias Kestenholz <mk@feinheit.ch>
2019-03-19 22:20:41 +0100

    Reformat

--- workbench/accounts/test_login.py
+++ workbench/accounts/test_login.py
@@ -20,7 +20,9 @@ class LoginTestCase(TestCase):
         self.client.login(email=user.email)
         self.client.cookies.load({"login_hint": "test@example.com"})
 
-        self.assertEqual(self.client.cookies.get("login_hint").value, "test@example.com")
+        self.assertEqual(
+            self.client.cookies.get("login_hint").value, "test@example.com"
+        )
         response = self.client.get("/accounts/logout/")
         self.assertRedirects(response, "/accounts/login/")
 
