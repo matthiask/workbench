ababda7f2b20edda6abc149564c07232f9771636
Matthias Kestenholz <mk@feinheit.ch>
2019-03-22 23:06:09 +0100

    Test aspects of the user model

---
+++ workbench/accounts/test_accounts.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from workbench.accounts.models import User
+
+
+class AccountsTest(TestCase):
+    def test_user(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user("", "")
+
+        user = User.objects.create_user("test@example.com", "")
+        self.assertFalse(user.has_usable_password())
+        self.assertTrue(user.has_perm("stuff"))
+        self.assertTrue(user.has_module_perms("stuff"))
+        self.assertFalse(user.is_staff)
+
+    def test_admin(self):
+        user = User.objects.create_superuser("test@example.com", "")
+        self.assertFalse(user.has_usable_password())
+        self.assertTrue(user.has_perm("stuff"))
+        self.assertTrue(user.has_module_perms("stuff"))
+        self.assertTrue(user.is_staff)
