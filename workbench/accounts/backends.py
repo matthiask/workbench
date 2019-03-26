28f40411cc223adfda60e5a0eec0eb4fc7366c3c
Matthias Kestenholz <mk@feinheit.ch>
2019-03-26 08:05:29 +0100

    Stop producing DoesNotExist exceptions for nothing here

--- workbench/accounts/backends.py
+++ workbench/accounts/backends.py
@@ -3,13 +3,8 @@ from workbench.accounts.models import User
 
 class AuthBackend(object):
     def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+        return User.objects.filter(pk=user_id).first()
 
     def authenticate(self, request, **kwargs):
-        try:
-            return User.objects.get(email=kwargs.get("email"))
-        except User.DoesNotExist:
-            pass
+        # Fine since email is unique
+        return User.objects.filter(email=kwargs.get("email")).first()
