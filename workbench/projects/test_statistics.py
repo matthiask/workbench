9282e8fe28ceaf71a88c500e54c50abfe63f6ac5
Matthias Kestenholz <mk@feinheit.ch>
2019-07-04 21:28:42 +0200

    Test not_archived_hours

--- workbench/projects/test_statistics.py
+++ workbench/projects/test_statistics.py
@@ -3,7 +3,9 @@ from decimal import Decimal
 from django.test import TestCase
 
 from workbench import factories
+from workbench.projects.models import Project
 from workbench.projects.reporting import overdrawn_projects
+from workbench.tools.models import Z
 
 
 class StatisticsTest(TestCase):
@@ -38,3 +40,41 @@ class StatisticsTest(TestCase):
         self.client.force_login(factories.UserFactory.create())
         response = self.client.get("/report/overdrawn-projects/")
         self.assertContains(response, "Überzogene Projekte")
+
+    def test_not_archived_hours(self):
+        service1 = factories.ServiceFactory.create(effort_rate=180, effort_type="Any")
+        service2 = factories.ServiceFactory.create(project=service1.project)
+
+        project = Project.objects.get()
+        self.assertEqual(
+            project.not_archived_total, {"total": Z, "hours_rate_undefined": Z}
+        )
+
+        factories.LoggedHoursFactory.create(service=service1, hours=10)
+        factories.LoggedHoursFactory.create(service=service2, hours=20)
+
+        project = Project.objects.get()
+        self.assertEqual(
+            project.not_archived_total,
+            {"total": Decimal("1800.00"), "hours_rate_undefined": Decimal("20.00")},
+        )
+
+        invoice = factories.InvoiceFactory.create(
+            project=project,
+            customer=project.customer,
+            contact=project.contact,
+            type=factories.Invoice.SERVICES,
+        )
+        invoice.create_services_from_logbook(project.services.all())
+
+        project = Project.objects.get()
+        self.assertEqual(
+            project.not_archived_total, {"total": Z, "hours_rate_undefined": Z}
+        )
+
+        factories.LoggedHoursFactory.create(service=service1, hours=10)
+        project = Project.objects.get()
+        self.assertEqual(
+            project.not_archived_total,
+            {"total": Decimal("1800.00"), "hours_rate_undefined": Z},
+        )
