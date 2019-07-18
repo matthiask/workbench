3ce14dc471c05a710bec7a586d22480188246423
Matthias Kestenholz <mk@feinheit.ch>
2019-07-18 09:01:03 +0200

    Minimally required HTML for project selection

--- workbench/logbook/views.py
+++ workbench/logbook/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect, render
-from django.utils.translation import gettext as _
 
 from workbench.projects.forms import ProjectAutocompleteForm
 
@@ -8,6 +7,4 @@ def createhours(request):
     form = ProjectAutocompleteForm(request.POST if request.method == "POST" else None)
     if form.is_valid():
         return redirect(form.cleaned_data["project"].urls["createhours"])
-    return render(
-        request, "modalform.html", {"form": form, "title": _("select a project")}
-    )
+    return render(request, "projects/select_project.html", {"form": form})
