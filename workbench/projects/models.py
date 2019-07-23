1a73e8534065c34314cd6007ded8d37ec6a14421
Matthias Kestenholz <mk@feinheit.ch>
2019-07-23 14:55:23 +0200

    Same dash

--- workbench/projects/models.py
+++ workbench/projects/models.py
@@ -74,7 +74,7 @@ class Project(Model):
         verbose_name_plural = _("projects")
 
     def __str__(self):
-        return "%s %s %s" % (self.code, self.title, self.owned_by.get_short_name())
+        return "%s %s - %s" % (self.code, self.title, self.owned_by.get_short_name())
 
     def __html__(self):
         return format_html(
