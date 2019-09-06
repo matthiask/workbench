8f4f36eb7ffa7c1a0e4e53dc876aa65ba789796c
Matthias Kestenholz <mk@feinheit.ch>
2019-09-06 11:30:22 +0200

    Automatically print the coverage after running tests

--- runtests.sh
+++ runtests.sh
@@ -2,4 +2,5 @@
 set -x
 venv/bin/coverage erase
 venv/bin/python -Wall venv/bin/coverage run --branch --include="*workbench*" --omit="*migrations*,*test_*,*venv*,*factories*" ./manage.py test -v 2 --keepdb $*
-venv/bin/coverage html
+# venv/bin/coverage html
+venv/bin/coverage report --skip-covered --show-missing
