f7ae459c0e1fda2026ffb041dbdb6089e403b625
Matthias Kestenholz <mk@feinheit.ch>
2019-09-15 13:28:29 +0200

    coverage does not detect a branch anymore

--- workbench/templatetags/workbench.py
+++ workbench/templatetags/workbench.py
@@ -149,18 +149,18 @@ def querystring(context, **kwargs):
 
 @register.simple_tag
 def history_link(instance):
-    if not instance.pk:
-        return ""
-
-    return format_html(
-        """
-        <a href="{}"
-            class="tiny-icons"
-            data-toggle="ajaxmodal"
-            title="{}">
+    return (
+        format_html(
+            """\
+<a href="{}"
+    class="tiny-icons"
+    data-toggle="ajaxmodal"
+    title="{}">
 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M13 3H7c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zm-1 8H8V5h4v6zM4 4h1v1H4v6h1v1H4c-.55 0-1-.45-1-1V5c0-.55.45-1 1-1zM1 5h1v1H1v4h1v1H1c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1z"/></svg>
-        </a>
-        """,  # noqa
-        reverse("history", args=(instance._meta.db_table, "id", instance.pk)),
-        _("History"),
+</a>""",  # noqa
+            reverse("history", args=(instance._meta.db_table, "id", instance.pk)),
+            _("History"),
+        )
+        if instance.pk
+        else ""
     )
