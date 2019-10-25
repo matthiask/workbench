c06eb938ca449bf65a2fee835d3a450496441309
Matthias Kestenholz <mk@feinheit.ch>
2019-10-25 14:12:06 +0200

    Stop localizing decimal fields

--- workbench/settings.py
+++ workbench/settings.py
@@ -123,7 +123,7 @@ LANGUAGE_CODE = "de"
 LANGUAGES = [("en", _("english")), ("de", _("german"))]
 TIME_ZONE = "Europe/Zurich"
 USE_I18N = True
-USE_L10N = True
+USE_L10N = False
 USE_TZ = True
 
 if LIVE:  # pragma: no cover
