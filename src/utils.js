3dc219dd52d8f5dc7240b535ab54edf9d858f454
Dan Büschlen <dab@feinheit.ch>
2019-11-05 14:50:59 +0100

    merge utils

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,26 @@ export function prettyDuration(secondsArgument) {
   const displaySeconds = (seconds % 60).toString().padStart(2, "0")
   return `${displayHours}${displayMinutes}:${displaySeconds}`
 }
+
+const snakeToCamel = string =>
+  string.replace(/(_\w)/g, match => match[1].toUpperCase())
+
+export function mapSnakeCaseToCamelCase(object) {
+  const camelCaseObject = {}
+
+  for (let key in object) {
+    camelCaseObject[snakeToCamel(key)] = object[key]
+  }
+
+  return camelCaseObject
+}
+
+export const containsJSON = response => {
+  const contentType = response.headers.get("content-type")
+
+  if (contentType && contentType.includes("application/json")) {
+    return true
+  } else {
+    return false
+  }
+}
