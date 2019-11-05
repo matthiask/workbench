705e81bb984922711b16417351d87a3378430f4a
Matthias Kestenholz <mk@feinheit.ch>
2019-11-05 13:50:44 +0100

    Draggable activities

--- src/activity.js
+++ src/activity.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Draggable from "react-draggable"
 
 export const Activity = _props => {
   const style = {
@@ -6,32 +7,34 @@ export const Activity = _props => {
     top: `${Math.floor(Math.random() * 100)}px`,
   }
   return (
-    <form className="activity" style={style}>
-      <div className="card">
-        <div className="card-header d-flex w-100 align-items-center justify-content-between">
-          <h5>Aktivität</h5>
-          <button className="btn btn-primary">&#x2056;</button>
-        </div>
-        <div className="card-body">
-          <div className="form-group">
-            <label>Projekt</label>
-            <input type="text" className="form-control" />
+    <Draggable>
+      <form className="activity" style={style}>
+        <div className="card">
+          <div className="card-header d-flex w-100 align-items-center justify-content-between">
+            <h5>Aktivität</h5>
+            <button className="btn btn-primary">&#x2056;</button>
           </div>
-          <div className="form-group">
-            <label>Leistung</label>
-            <input type="text" className="form-control" />
+          <div className="card-body">
+            <div className="form-group">
+              <label>Projekt</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>Leistung</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>Tätigkeit</label>
+              <textarea className="form-control" rows="3" />
+            </div>
+            <span>0:13:29</span>
           </div>
-          <div className="form-group">
-            <label>Tätigkeit</label>
-            <textarea className="form-control" rows="3" />
+          <div className="card-footer d-flex justify-content-between">
+            <button className="btn btn-success">Pause</button>
+            <button className="btn btn-primary">Send</button>
           </div>
-          <span>0:13:29</span>
         </div>
-        <div className="card-footer d-flex justify-content-between">
-          <button className="btn btn-success">Pause</button>
-          <button className="btn btn-primary">Send</button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </Draggable>
   )
 }
