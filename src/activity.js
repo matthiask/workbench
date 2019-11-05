eaf1a41641dad70d89dc751104c8cb4a954a3950
Matthias Kestenholz <mk@feinheit.ch>
2019-11-05 12:21:12 +0100

    Form field groups

--- src/activity.js
+++ src/activity.js
@@ -6,12 +6,18 @@ export const Activity = _props => {
       <div className="card">
         <div className="card-header"></div>
         <div className="card-body">
-          <label>Projekt</label>
-          <input type="text" />
-          <label>Leistung</label>
-          <input type="text" />
-          <label>Tätigkeit</label>
-          <input type="text" />
+          <div className="form-group">
+            <label>Projekt</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Leistung</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Tätigkeit</label>
+            <textarea className="form-control" rows="3" />
+          </div>
           <span>0:13:29</span>
         </div>
         <div className="card-footer">
