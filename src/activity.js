917430217c3b64ce19137c72c899a2222b817d0d
Matthias Kestenholz <mk@feinheit.ch>
2019-11-05 14:44:41 +0100

    Start with settings

--- src/activity.js
+++ src/activity.js
@@ -1,22 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import Draggable from "react-draggable"
 
 import {prettyDuration} from "./utils.js"
 
-export const Activity = ({description, seconds}) => {
-  const style = {
-    left: `${Math.floor(Math.random() * 500)}px`,
-    top: `${Math.floor(Math.random() * 100)}px`,
-  }
+export const Activity = ({description, seconds, top, left}) => {
+  const [showSettings, setShowSettings] = useState(false)
+  const [color, setColor] = useState("#e3f2fd")
+
+  const style = {backgroundColor: color}
   return (
-    <Draggable handle=".js-drag-handle">
-      <form className="activity" style={style}>
+    <Draggable
+      handle=".js-drag-handle"
+      onStop={(e, data) => {
+        console.log(e, data)
+      }}
+    >
+      <form className="activity">
         <div className="card">
           <div className="card-header d-flex w-100 align-items-center justify-content-between js-drag-handle">
             <h5>Aktivität</h5>
-            <button className="btn btn-primary">&#x2056;</button>
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={() => setShowSettings(!showSettings)}
+            >
+              &#x2056;
+            </button>
           </div>
-          <div className="card-body">
+          <div className="card-body" style={style}>
+            {showSettings ? (
+              <div className="activity-settings">
+                <input
+                  type="color"
+                  value={color}
+                  onChange={e => setColor(e.target.value)}
+                />
+              </div>
+            ) : null}
             <div className="form-group">
               <label>Projekt</label>
               <input type="text" className="form-control" />
