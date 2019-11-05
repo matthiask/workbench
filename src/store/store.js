99230171f202d663e8279bb8a0e6d8fa108f3e0a
Dan Büschlen <dab@feinheit.ch>
2019-11-05 13:51:19 +0100

    add localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,20 @@
 import React from "react"
 
-import {createStore, applyMiddleware} from "redux"
+import {createStore, applyMiddleware, compose} from "redux"
 import reducer from "./reducers"
 import thunk from "redux-thunk"
 import logger from "redux-logger"
+import persistState from "redux-localstorage"
 
 export function configureStore(initialState = undefined) {
-  let store = createStore(reducer, initialState, applyMiddleware(thunk, logger))
+  let store = createStore(
+    reducer,
+    initialState,
+    compose(
+      persistState(),
+      applyMiddleware(thunk, logger)
+    )
+  )
 
   if (module.hot) {
     module.hot.accept(() => {
