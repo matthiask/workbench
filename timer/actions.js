df88c5e4c30f1a9a0f9c0ef444093cfab716b90b
Matthias Kestenholz <mk@feinheit.ch>
2019-11-08 11:30:45 +0100

    Commit this

--- timer/actions.js
+++ timer/actions.js
@@ -15,6 +15,26 @@ export function createActivity(dispatch, fields = {}) {
   })
 }
 
+export async function fetchProjects(q) {
+  const url = endpointUrl({name: "projects", urlParams: [q]})
+  const response = await fetch(url, {credentials: "include"})
+  if (containsJSON(response)) {
+    const data = await response.json()
+    return data.results
+  }
+  return []
+}
+
+export async function fetchServices(project) {
+  const url = endpointUrl({name: "services", urlParams: [project]})
+  const response = await fetch(url, {credentials: "include"})
+  if (containsJSON(response)) {
+    const data = await response.json()
+    return data.services.map(row => ({label: row[1], value: row[0]}))
+  }
+  return []
+}
+
 export async function loadProjects(dispatch) {
   const response = await fetch(endpointUrl({name: "activeProjects"}), {
     credentials: "include",
