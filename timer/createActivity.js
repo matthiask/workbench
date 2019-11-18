8406e10b5506fcfe7fc3ecb63164b5356e3748ba
Matthias Kestenholz <mk@feinheit.ch>
2019-11-18 16:21:53 +0100

    Remove the input field from the header bar

--- timer/createActivity.js
+++ timer/createActivity.js
@@ -1,35 +1,19 @@
-import React, {useState} from "react"
+import React from "react"
 import {connect} from "react-redux"
 
 import {createActivity} from "./actions.js"
-import {gettext, OUTCOME} from "./i18n.js"
+import {gettext} from "./i18n.js"
 
 export const CreateActivity = connect()(({dispatch}) => {
-  const [description, setDescription] = useState("")
   return (
-    <form
-      className="form-inline create-activity"
-      onSubmit={e => {
-        e.preventDefault()
-        createActivity(dispatch, {description})
-        setDescription("")
+    <button
+      className="btn btn-primary"
+      type="button"
+      onClick={() => {
+        createActivity(dispatch)
       }}
     >
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control"
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-          placeholder={OUTCOME}
-          style={{minWidth: "15rem"}}
-        />
-        <div className="input-group-append">
-          <button className="btn btn-primary" type="submit">
-            {gettext("Start")}!
-          </button>
-        </div>
-      </div>
-    </form>
+      +{gettext("Timer")}
+    </button>
   )
 })
