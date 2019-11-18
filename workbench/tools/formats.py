b69ed61ccd97f204b6746562f7dcf7ec07918a79
Matthias Kestenholz <mk@feinheit.ch>
2019-11-18 21:09:46 +0100

    Remove pretty_due

--- workbench/tools/formats.py
+++ workbench/tools/formats.py
@@ -3,7 +3,6 @@ from decimal import Decimal
 
 from django.utils.formats import date_format
 from django.utils.timezone import localtime
-from django.utils.translation import gettext as _
 
 
 H1 = Decimal("0.0")
@@ -18,20 +17,6 @@ def local_date_format(dttm, fmt=None):
     return date_format(
         dttm, fmt or ("d.m.Y H:i" if isinstance(dttm, dt.datetime) else "d.m.Y")
     )
-
-
-def pretty_due(day):
-    days = (day - dt.date.today()).days
-    if days > 14:
-        return _("due in %s weeks") % (days // 7)
-    elif days > 1:
-        return _("due in %s days") % days
-    elif days == 1:
-        return _("due tomorrow")
-    elif days == 0:
-        return _("due today")
-    else:
-        return _("overdue!")
 
 
 def currency(value, show_plus_sign=False):
