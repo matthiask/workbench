5fe45c7a41bf282a26fcf938d60dc8592b6dc6bd
Matthias Kestenholz <mk@feinheit.ch>
2019-12-26 07:36:23 +0100

    Send accounting files to admins

--- workbench/management/commands/fairy_tasks.py
+++ workbench/management/commands/fairy_tasks.py
@@ -6,6 +6,7 @@ from django.core.management import BaseCommand
 from django.utils.translation import activate
 
 from workbench.accounts.middleware import set_user_name
+from workbench.accounts.models import User
 from workbench.accruals.models import Accrual
 from workbench.accruals.tasks import create_accruals_for_last_month
 from workbench.invoices.tasks import create_recurring_invoices_and_notify
@@ -37,7 +38,14 @@ class Command(BaseCommand):
         )
         xlsx.accruals(Accrual.objects.filter(cutoff_date=today - dt.timedelta(days=1)))
 
-        mail = EmailMessage("Accounting files", to=["mk@feinheit.ch"])
+        mail = EmailMessage(
+            "Accounting files",
+            to=list(
+                User.objects.filter(is_active=True, is_admin=True).values_list(
+                    "email", flat=True
+                )
+            ),
+        )
         with io.BytesIO() as buf:
             xlsx.workbook.save(buf)
             mail.attach(
