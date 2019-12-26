620a6732b84d919a4cb1945cfe49a10e0020389a
Matthias Kestenholz <mk@feinheit.ch>
2019-12-26 12:49:24 +0100

    Configure coverage using setup.cfg

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -x
 venv/bin/coverage erase
-venv/bin/python -Wall venv/bin/coverage run --branch --include="*workbench*" --omit="*migrations*,*test_*,*venv*,*factories*" ./manage.py test -v 2 --keepdb $*
-# venv/bin/coverage html
-venv/bin/coverage report --skip-covered --show-missing
+venv/bin/python -Wall venv/bin/coverage run ./manage.py test -v 2 --keepdb $*
+venv/bin/coverage report
