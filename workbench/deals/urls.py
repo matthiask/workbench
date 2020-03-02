05fe830e91db094e72cbc96f39ef6d4fdf5fe958
Matthias Kestenholz <mk@feinheit.ch>
2020-03-02 21:22:45 +0100

    Deals are CONTROLLING only

--- workbench/deals/urls.py
+++ workbench/deals/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from workbench import generic
+from workbench.accounts.features import controlling_only
 from workbench.deals.forms import DealForm, DealSearchForm, SetStatusForm
 from workbench.deals.models import Deal
 
@@ -8,34 +9,38 @@ from workbench.deals.models import Deal
 urlpatterns = [
     url(
         r"^$",
-        generic.ListView.as_view(model=Deal, search_form_class=DealSearchForm),
+        controlling_only(
+            generic.ListView.as_view(model=Deal, search_form_class=DealSearchForm)
+        ),
         name="deals_deal_list",
     ),
     url(
         r"^(?P<pk>\d+)/$",
-        generic.DetailView.as_view(model=Deal),
+        controlling_only(generic.DetailView.as_view(model=Deal)),
         name="deals_deal_detail",
     ),
     url(
         r"^create/$",
-        generic.CreateView.as_view(form_class=DealForm, model=Deal),
+        controlling_only(generic.CreateView.as_view(form_class=DealForm, model=Deal)),
         name="deals_deal_create",
     ),
     url(
         r"^(?P<pk>\d+)/update/$",
-        generic.UpdateView.as_view(form_class=DealForm, model=Deal),
+        controlling_only(generic.UpdateView.as_view(form_class=DealForm, model=Deal)),
         name="deals_deal_update",
     ),
     url(
         r"^(?P<pk>\d+)/set-status/$",
-        generic.UpdateView.as_view(
-            form_class=SetStatusForm, model=Deal, template_name="modalform.html"
+        controlling_only(
+            generic.UpdateView.as_view(
+                form_class=SetStatusForm, model=Deal, template_name="modalform.html"
+            )
         ),
         name="deals_deal_set_status",
     ),
     url(
         r"^(?P<pk>\d+)/delete/$",
-        generic.DeleteView.as_view(model=Deal),
+        controlling_only(generic.DeleteView.as_view(model=Deal)),
         name="deals_deal_delete",
     ),
 ]
