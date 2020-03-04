9e625b81cd9292fef7583a6129194052518f17d1
Matthias Kestenholz <mk@feinheit.ch>
2020-03-04 09:31:35 +0100

    Add stats to logged hours export

--- workbench/tools/xlsx.py
+++ workbench/tools/xlsx.py
@@ -1,29 +1,67 @@
+from collections import defaultdict
+from itertools import chain
+
 from django.utils.translation import gettext as _
 
 from xlsxdocument import XLSXDocument
 
 from workbench.contacts.models import PostalAddress
+from workbench.tools.formats import H1
 
 
 class WorkbenchXLSXDocument(XLSXDocument):
     def logged_hours(self, queryset):
+        queryset = queryset.select_related(
+            "created_by",
+            "rendered_by",
+            "service__project__owned_by",
+            "invoice_service__invoice__owned_by",
+            "invoice_service__invoice__project",
+        )
+
         self.table_from_queryset(
-            queryset.select_related(
-                "created_by",
-                "rendered_by",
-                "service__project__owned_by",
-                "invoice_service__invoice__owned_by",
-                "invoice_service__invoice__project",
-            ),
+            queryset,
             additional=[
-                ("service", lambda hours: hours.service),
-                ("project", lambda hours: hours.service.project),
+                (_("hourly rate"), lambda hours: hours.service.effort_rate),
+                (_("project"), lambda hours: hours.service.project),
                 (
-                    "invoice",
+                    _("invoice"),
                     lambda hours: hours.invoice_service.invoice
                     if hours.invoice_service
                     else None,
                 ),
+            ],
+        )
+
+        by_service_and_user = defaultdict(lambda: defaultdict(lambda: H1))
+
+        for h in queryset:
+            by_service_and_user[h.service][h.rendered_by] += h.hours
+
+        self.add_sheet(_("by service and user"))
+        users = sorted(
+            set(
+                chain.from_iterable(
+                    users.keys() for users in by_service_and_user.values()
+                )
+            )
+        )
+
+        self.table(
+            [_("project"), _("service"), _("hourly rate"), _("total")]
+            + [user.get_short_name() for user in users],
+            [
+                [
+                    service.project,
+                    service,
+                    service.effort_rate,
+                    sum(by_users.values(), H1),
+                ]
+                + [by_users.get(user) for user in users]
+                for service, by_users in sorted(
+                    by_service_and_user.items(),
+                    key=lambda row: (row[0].project_id, row[0].position),
+                )
             ],
         )
 
