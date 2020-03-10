a78158f6b335748a9cbea4f8632e61952262a81b
Matthias Kestenholz <mk@feinheit.ch>
2020-03-10 18:20:36 +0100

    Calculate labor costs by user

--- workbench/reporting/labor_costs.py
+++ workbench/reporting/labor_costs.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 from django.db import connections
 
+from workbench.accounts.models import User
 from workbench.projects.models import Project
 from workbench.tools.models import Z
 
@@ -11,6 +12,7 @@ select
     ps.project_id,
     hourly_labor_costs,
     green_hours_target,
+    lh.rendered_by_id,
     sum(lh.hours) as hours
 from logbook_loggedhours lh
 left join projects_service ps on lh.service_id=ps.id
@@ -19,7 +21,7 @@ lh.rendered_by_id=costs.user_id
 and lh.rendered_on >= costs.date_from
 and lh.rendered_on <= costs.date_until
 %s
-group by ps.project_id, hourly_labor_costs, green_hours_target
+group by ps.project_id, hourly_labor_costs, green_hours_target, lh.rendered_by_id
 """
 
 
@@ -30,22 +32,37 @@ def labor_costs(date_range):
             "hours_with_rate_undefined": Z,
             "costs": Z,
             "costs_with_green_hours_target": Z,
+            "by_user": defaultdict(
+                lambda: {"hours": Z, "costs": Z, "costs_with_green_hours_target": Z}
+            ),
         }
     )
+
+    users = {u.id: u for u in User.objects.all()}
 
     with connections["default"].cursor() as cursor:
         cursor.execute(
             SQL % "where lh.rendered_on >= %s and lh.rendered_on <= %s", date_range
         )
 
-        for project_id, hlc, ght, hours in cursor:
+        for project_id, hlc, ght, rendered_by_id, hours in cursor:
             project = projects[project_id]
             project["hours"] += hours
+
+            by_user = project["by_user"][users.get(rendered_by_id, rendered_by_id)]
+            by_user["hours"] += hours
+
             if hlc is None:
                 project["hours_with_rate_undefined"] += hours
             else:
-                project["costs"] += hours * hlc
-                project["costs_with_green_hours_target"] += hours * hlc * 100 / ght
+                costs = hours * hlc
+                costs_with_ght = hours * hlc * 100 / ght
+
+                project["costs"] += costs
+                project["costs_with_green_hours_target"] += costs_with_ght
+
+                by_user["costs"] += costs
+                by_user["costs_with_green_hours_target"] += costs_with_ght
 
     return [
         {"project": project, **projects[project.id]}
@@ -53,3 +70,10 @@ def labor_costs(date_range):
             "owned_by"
         )
     ]
+
+
+def test():  # pragma: no cover
+    from pprint import pprint
+    import datetime as dt
+
+    pprint(labor_costs([dt.date(2019, 1, 1), dt.date.today()]))
