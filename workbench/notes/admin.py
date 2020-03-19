3025ab561cf2768f94bb032f3c32435b3a7bdb54
Matthias Kestenholz <mk@feinheit.ch>
2020-03-19 17:44:34 +0100

    Better notes changelist

--- workbench/notes/admin.py
+++ workbench/notes/admin.py
@@ -5,4 +5,5 @@ from . import models
 
 @admin.register(models.Note)
 class NoteAdmin(admin.ModelAdmin):
+    list_display = ["title", "content_object", "created_by", "created_at"]
     search_fields = ["title", "description"]
