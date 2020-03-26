b8851ea7e3b39b3023bd4afc33f4d7de7c25d3a8
Matthias Kestenholz <mk@feinheit.ch>
2020-03-26 09:28:04 +0100

    Make this usable

--- workbench/timer/admin.py
+++ workbench/timer/admin.py
@@ -24,3 +24,5 @@ class TimestampAdmin(admin.ModelAdmin):
     date_hierarchy = "created_at"
     list_display = ["user", "created_at", "type", "notes"]
     list_filter = [("user", admin.RelatedOnlyFieldListFilter)]
+    radio_fields = {"type": admin.HORIZONTAL}
+    raw_id_fields = ["logged_hours"]
