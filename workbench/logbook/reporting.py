14774e9aa525bdf3e42e857769680ee3dc6f9abc
Matthias Kestenholz <mk@feinheit.ch>
2020-03-29 16:18:01 +0200

    Report days with insufficient breaks

--- workbench/logbook/reporting.py
+++ workbench/logbook/reporting.py
@@ -69,9 +69,68 @@ GROUP BY rendered_by_id
         }
 
 
+def insufficient_breaks(date_range):
+    with connections["default"].cursor() as cursor:
+        cursor.execute(
+            """
+with breaks as (
+    select
+        user_id,
+        day,
+        sum(extract(epoch from (ends_at - starts_at))) as break_seconds
+    from logbook_break
+    where day between %s and %s
+    group by user_id, day
+),
+logged_hours as (
+    select
+        rendered_by_id as user_id,
+        rendered_on as day,
+        sum(hours) as hours
+    from logbook_loggedhours
+    where rendered_on between %s and %s
+    group by user_id, day
+),
+combined as (
+    select
+        logged_hours.user_id,
+        logged_hours.day,
+        hours,
+        coalesce(break_seconds, 0) as break_seconds
+    from logged_hours
+    left join breaks
+    on
+        breaks.user_id=logged_hours.user_id
+        and breaks.day=logged_hours.day
+),
+insufficient_breaks as (
+    select * from combined where hours >= 5.5 and hours < 7 and break_seconds < 900
+    union all
+    select * from combined where hours >= 7.0 and hours < 9 and break_seconds < 1800
+    union all
+    select * from combined where hours >= 9 and break_seconds < 3600
+)
+select
+    user_id,
+    count(*) as count,
+    (
+        select count(*)
+        from logged_hours
+        where insufficient_breaks.user_id=logged_hours.user_id
+    )
+from insufficient_breaks
+group by user_id
+            """,
+            [*date_range, *date_range],
+        )
+
+        return {user_id: {"days": days, "of": of} for user_id, days, of in cursor}
+
+
 def logbook_stats(date_range):
     mld = mean_logging_delay(date_range)
     lhs = logged_hours_stats(date_range)
+    ib = insufficient_breaks(date_range)
 
     users = User.objects.filter(id__in=mld.keys() | lhs.keys())
 
@@ -80,6 +139,7 @@ def logbook_stats(date_range):
             "user": user,
             "mean_logging_delay": mld[user.id],
             "logged_hours_stats": lhs[user.id],
+            "insufficient_breaks": ib.get(user.id, 0),
         }
         for user in users
     ]
@@ -93,5 +153,5 @@ def test():  # pragma: no cover
 
     # pprint(mean_logging_delay(date_range))
     # pprint(logged_hours_stats(date_range))
-
+    # pprint(insufficient_breaks(date_range))
     pprint(logbook_stats(date_range))
