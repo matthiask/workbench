05f9eaafa18cacfb9958cc1a3cd81554ba27ca16
Matthias Kestenholz <mk@feinheit.ch>
2020-05-26 09:07:56 +0200

    CORS: Also allow list-timestamps

--- workbench/settings.py
+++ workbench/settings.py
@@ -366,7 +366,7 @@ GLASSFROG_TOKEN = env("GLASSFROG_TOKEN", warn=True)
 
 # Fine since it's only used for selected views
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_URLS_REGEX = r"^/create-timestamp/.*$"  # Better safe than sorry
+CORS_URLS_REGEX = r"^/(create-timestamp|list-timestamps)/.*$"  # Better safe than sorry
 
 if DEBUG:  # pragma: no cover
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
