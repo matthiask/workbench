1fde3e27fc26cc2fca9a7b5707951b5d7abed598
Matthias Kestenholz <mk@feinheit.ch>
2020-07-01 14:12:56 +0200

    Test the planning search forms a bit

--- workbench/planning/test_planning.py
+++ workbench/planning/test_planning.py
@@ -1,11 +1,13 @@
 import datetime as dt
 
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.utils.translation import deactivate_all
 
 from workbench import factories
 from workbench.planning import reporting
+from workbench.planning.forms import PlannedWorkSearchForm, PlanningRequestSearchForm
+from workbench.planning.models import PlannedWork, PlanningRequest
 from workbench.tools.validation import monday
 
 
@@ -62,3 +64,30 @@ class PlanningTest(TestCase):
         report = reporting.project_planning(pw.project)
         self.assertEqual(sum(report["by_week"]), 20)
         self.assertEqual(len(report["projects_offers"]), 1)
+
+    def test_planning_search_forms(self):
+        """Planning request search form branch test"""
+
+        project = factories.ProjectFactory.create()
+        pr = factories.PlanningRequestFactory.create()
+        pw = factories.PlannedWorkFactory.create(weeks=[dt.date(2020, 6, 22)])
+
+        rf = RequestFactory()
+
+        for form_class, model, instances in [
+            (PlanningRequestSearchForm, PlanningRequest, [pr]),
+            (PlannedWorkSearchForm, PlannedWork, [pw]),
+        ]:
+            req = rf.get("/")
+            req.user = project.owned_by
+            form = form_class(req.GET, request=req)
+            self.assertTrue(form.is_valid())
+            queryset = form.filter(model.objects.all())
+            self.assertEqual(list(queryset), instances)
+
+            req = rf.get("/?project={}".format(project.pk))
+            req.user = project.owned_by
+            form = form_class(req.GET, request=req)
+            self.assertTrue(form.is_valid())
+            queryset = form.filter(model.objects.all())
+            self.assertEqual(list(queryset), [])
