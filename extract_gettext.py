145f8ff6729b8dc3f2a7054775472bc4c142e8b5
Matthias Kestenholz <mk@feinheit.ch>
2021-01-06 08:30:00 +0100

    Commit the improved extract_gettext.py script

--- extract_gettext.py
+++ extract_gettext.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+Extract strings::
+
+    python3 extract_gettext.py
+
+Run tests::
+
+    python3 -m doctest -v extract_gettext.py
+
+"""
+
 import re
 import subprocess
+from collections import deque
 
 
 def js_files():
@@ -13,19 +25,58 @@ def js_files():
     return res.stdout.splitlines()
 
 
-def gettext_calls(file):
-    with open(file, encoding="utf-8") as f:
-        return [
-            f"{match[0]}({match[1]})"
-            for match in re.findall(
-                r"""\b(\w*gettext)\(\s*((['"]).+?\3)\s*\)""",
-                f.read(),
-            )
-        ]
+def extract_args(part):
+    parens = 0
+    quote = ""
+    for idx, c in enumerate(part):
+        if c == quote:
+            quote = ""
+        elif c in {"'", '"'}:
+            quote = c
+        elif quote:
+            pass
+        elif c == "(":
+            parens += 1
+        elif c == ")":
+            parens -= 1
+
+        if parens == 0:
+            return part[1:idx]
+
+    return part
+
+
+def gettext_calls(source):
+    """Extract *gettext calls from code
+
+    >>> list(gettext_calls("gettext('abc')"))
+    ["gettext('abc')"]
+    >>> list(gettext_calls("abc def gettext('abc') xyz gettext blub"))
+    ["gettext('abc')"]
+    >>> list(gettext_calls("abc ngettext('singular', 'plural', someVar) def"))
+    ["ngettext('singular', 'plural', someVar)"]
+    >>> list(gettext_calls("abc def gettext ( ' abc ' ) xyz"))
+    ["gettext(' abc ')"]
+    >>> list(gettext_calls("gettext(':-/')"))
+    ["gettext(':-/')"]
+    >>> list(gettext_calls("gettext(':-)')"))
+    ["gettext(':-)')"]
+    """
+
+    parts = deque(part.strip() for part in re.split(r"\b(\w*gettext)\b", source))
+
+    while parts:
+        top = parts.popleft()
+        if not top.endswith("gettext"):
+            continue
+
+        if parts and (args := extract_args(parts.popleft())):
+            yield f"{top}({args.strip()})"
 
 
 if __name__ == "__main__":
-    calls = []
+    calls = set()
     for file in js_files():
-        calls.extend(gettext_calls(file))
-    print("\n".join(sorted(set(calls))))
+        with open(file, encoding="utf-8") as f:
+            calls |= set(gettext_calls(f.read()))
+    print("\n".join(sorted(calls, key=lambda c: c.lower())))
