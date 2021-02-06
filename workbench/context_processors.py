3f6dd35bd19b4f717eab6ebd7228c08c8b894e18
Matthias Kestenholz <mk@feinheit.ch>
2021-02-06 11:30:35 +0100

    ✨

--- workbench/context_processors.py
+++ workbench/context_processors.py
@@ -1,12 +1,16 @@
+import datetime as dt
+
 from django.conf import settings
 
 from workbench.accounts.features import FEATURES
 
 
 def workbench(request):
+    today = dt.date.today()
     return {
         "WORKBENCH": settings.WORKBENCH,
         "FEATURES": FEATURES,
         "DEBUG": settings.DEBUG,
         "TESTING": settings.TESTING,
+        "SPARKLES": (today.month, today.day) == (3, 8),
     }
