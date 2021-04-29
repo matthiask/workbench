c349b8a10b6cf92bccf3cc536cf1b3b5d5409f7f
Matthias Kestenholz <mk@feinheit.ch>
2021-04-29 12:35:29 +0200

    Extract more fields

--- workbench/planning/updates.py
+++ workbench/planning/updates.py
@@ -6,6 +6,13 @@ from workbench.audit.models import LoggedAction, audit_user_id
 from workbench.planning.models import Milestone, PlannedWork
 
 
+def tryint(str):
+    try:
+        return int(str)
+    except (TypeError, ValueError):
+        return None
+
+
 def updated():
     queryset = LoggedAction.objects.filter(
         created_at__gte=timezone.now() - dt.timedelta(hours=48)
@@ -19,10 +26,17 @@ def updated():
                     "by": audit_user_id(row["user_name"]),
                     "id": int(row["row_data__id"]),
                     "user_id": int(row["row_data__user_id"]),
+                    "created_by_id": tryint(row["row_data__created_by_id"]),
+                    "project_id": int(row["row_data__project_id"]),
                     "action": row["action"],
                 }
                 for row in queryset.for_model(PlannedWork).values(
-                    "user_name", "action", "row_data__id", "row_data__user_id"
+                    "user_name",
+                    "action",
+                    "row_data__id",
+                    "row_data__user_id",
+                    "row_data__created_by_id",
+                    "row_data__project_id",
                 )
             ]
             if row["by"] != row["user_id"]
@@ -31,10 +45,14 @@ def updated():
             {
                 "by": audit_user_id(row["user_name"]),
                 "id": int(row["row_data__id"]),
+                "project_id": int(row["row_data__project_id"]),
                 "action": row["action"],
             }
             for row in queryset.for_model(Milestone).values(
-                "user_name", "action", "row_data__id"
+                "user_name",
+                "action",
+                "row_data__id",
+                "row_data__project_id",
             )
         ],
     }
