8f5fb883f0932c58679be7c3a8bfa71dc6ecf676
Matthias Kestenholz <mk@feinheit.ch>
2021-04-29 16:57:46 +0200

    Outline a possible method to generate a list of projects where plans have changed

--- workbench/planning/updates.py
+++ workbench/planning/updates.py
@@ -1,9 +1,12 @@
 import datetime as dt
+from collections import defaultdict
 
+from django.db.models import Q
 from django.utils import timezone
 
+from workbench.accounts.models import User
 from workbench.audit.models import LoggedAction, audit_user_id
-from workbench.planning.models import Milestone, PlannedWork
+from workbench.projects.models import Project
 
 
 def tryint(str):
@@ -15,47 +18,22 @@ def tryint(str):
 
 def updated():
     queryset = LoggedAction.objects.filter(
-        created_at__gte=timezone.now() - dt.timedelta(hours=48)
-    )
+        created_at__gte=timezone.now() - dt.timedelta(hours=24),
+        table_name__in=["planning_milestone", "planning_plannedwork"],
+    ).values("user_name", "row_data__project_id")
+    users = {user.id: user for user in User.objects.all()} | {None: None}
 
-    return {
-        "planned_work": [
-            row
-            for row in [
-                {
-                    "by": audit_user_id(row["user_name"]),
-                    "id": int(row["row_data__id"]),
-                    "user_id": int(row["row_data__user_id"]),
-                    "created_by_id": tryint(row["row_data__created_by_id"]),
-                    "project_id": int(row["row_data__project_id"]),
-                    "action": row["action"],
-                }
-                for row in queryset.for_model(PlannedWork).values(
-                    "user_name",
-                    "action",
-                    "row_data__id",
-                    "row_data__user_id",
-                    "row_data__created_by_id",
-                    "row_data__project_id",
-                )
-            ]
-            if row["by"] != row["user_id"]
-        ],
-        "milestones": [
-            {
-                "by": audit_user_id(row["user_name"]),
-                "id": int(row["row_data__id"]),
-                "project_id": int(row["row_data__project_id"]),
-                "action": row["action"],
-            }
-            for row in queryset.for_model(Milestone).values(
-                "user_name",
-                "action",
-                "row_data__id",
-                "row_data__project_id",
-            )
-        ],
-    }
+    projects = defaultdict(list)
+    for row in queryset:
+        pid = int(row["row_data__project_id"])
+        projects[pid].append(users[audit_user_id(row["user_name"])])
+
+    return [
+        {"project": project, "updated_by": projects[project.id]}
+        for project in Project.objects.open()
+        .select_related("owned_by")
+        .filter(Q(id__in=projects) & ~Q(type=Project.INTERNAL))
+    ]
 
 
 def test():  # pragma: no cover
