cac6ccf61428c5990146a24a552b5520c2c03111
Matthias Kestenholz <mk@feinheit.ch>
2021-10-20 09:07:35 +0200

    Only run coverage report when no arguments have been given

--- runtests.sh
+++ runtests.sh
@@ -2,4 +2,7 @@
 set -x
 venv/bin/coverage erase
 venv/bin/python -Wall venv/bin/coverage run ./manage.py test -v 2 --keepdb $*
-venv/bin/coverage report
+
+if [ $# -eq 0 ]; then
+  venv/bin/coverage report
+fi
