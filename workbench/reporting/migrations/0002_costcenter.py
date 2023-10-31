35061f61c15d9d8433bbf84d0658ee23b9d79f0a
Matthias Kestenholz <mk@feinheit.ch>
2023-10-31 14:03:50 +0100

    This dependency exists

--- workbench/reporting/migrations/0002_costcenter.py
+++ workbench/reporting/migrations/0002_costcenter.py
@@ -5,6 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
+        ("audit", "0001_initial"),
         ("reporting", "0001_initial"),
     ]
 
