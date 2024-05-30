31cb39afc14969fad1c678d3f484db067ce3d296
Matthias Kestenholz <mk@feinheit.ch>
2024-05-30 17:07:29 +0200

    Fix the test running script

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -x
-venv/bin/coverage erase
-venv/bin/python -Wall venv/bin/coverage run ./manage.py test -v 2 --keepdb $*
+set -ex
+.venv/bin/coverage erase
+.venv/bin/python -Wall .venv/bin/coverage run ./manage.py test -v 2 --keepdb $*
 
 if [ $# -eq 0 ]; then
-  venv/bin/coverage report
+  .venv/bin/coverage report
 fi
