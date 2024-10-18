975fac1da4f27ef22879cbb313ecef67a1fa128b
Matthias Kestenholz <mk@feinheit.ch>
2024-10-18 09:18:53 +0200

    Updates

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import fh_fablib as fl
 
 
-fl.require("1.0.20240904")
+fl.require("1.0.20241002")
 fl.config.update(
     app="workbench", domain="workbench.feinheit.ch", branch="main", remote="production"
 )
